8443a7b4a7ca30aebfee06b93da37d92e5e1cb42
Tshaka Lekholoane <mail+git@tshaka.dev>
2026-03-14 11:50:13 +0200

    etc/nvim: remove auto saving augroup

--- etc/.config/nvim/lua/autocommands.lua
+++ etc/.config/nvim/lua/autocommands.lua
@@ -2,19 +2,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank()
   end,
-  desc     = "Highlight on yank",
-  group    = vim.api.nvim_create_augroup("highlight-on-yank", { clear = true }),
-  pattern  = "*",
-})
-
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "VimLeavePre" }, {
-  callback = function()
-    if vim.bo.buftype ~= "" then
-      return
-    end
-    vim.cmd.update()
-  end,
-  desc     = "Autosave",
-  group    = vim.api.nvim_create_augroup("autosave", { clear = true }),
-  pattern  = "*",
+  desc = "Highlight on yank",
+  group = vim.api.nvim_create_augroup("highlight-on-yank", { clear = true }),
+  pattern = "*",
 })
